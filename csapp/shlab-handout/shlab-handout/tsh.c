0dbac845e8650db918891d3b8d3e5d2c6be7662d
1120200603 <971843634@qq.com>
2025-01-02 12:07:58 +0800

    翻译成中文

--- csapp/shlab-handout/shlab-handout/tsh.c
+++ csapp/shlab-handout/shlab-handout/tsh.c
@@ -1,7 +1,7 @@
-/* 
- * tsh - A tiny shell program with job control
- * 
- * <Put your name and login ID here>
+/*
+ * tsh - 一个带有作业控制的微型shell程序
+ *
+ * <在这里输入你的名字和登录ID>
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,48 +13,48 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-/* Misc manifest constants */
-#define MAXLINE    1024   /* max line size */
-#define MAXARGS     128   /* max args on a command line */
-#define MAXJOBS      16   /* max jobs at any point in time */
-#define MAXJID    1<<16   /* max job ID */
+/* 其他常数 */
+#define MAXLINE    1024   /* 最大行大小 */
+#define MAXARGS     128   /* 命令行上的最大参数数量 */
+#define MAXJOBS      16   /* 任何时候最多的作业数量 */
+#define MAXJID    1<<16   /* 最大作业ID */
 
-/* Job states */
-#define UNDEF 0 /* undefined */
-#define FG 1    /* running in foreground */
-#define BG 2    /* running in background */
-#define ST 3    /* stopped */
+/* 作业状态 */
+#define UNDEF 0 /* 未定义 */
+#define FG 1    /* 在前台运行 */
+#define BG 2    /* 在后台运行 */
+#define ST 3    /* 已停止 */
 
-/* 
- * Jobs states: FG (foreground), BG (background), ST (stopped)
- * Job state transitions and enabling actions:
+/*
+ * 作业状态：FG（前台），BG（后台），ST（已停止）
+ * 作业状态转换和启用动作：
  *     FG -> ST  : ctrl-z
- *     ST -> FG  : fg command
- *     ST -> BG  : bg command
- *     BG -> FG  : fg command
- * At most 1 job can be in the FG state.
+ *     ST -> FG  : fg命令
+ *     ST -> BG  : bg命令
+ *     BG -> FG  : fg命令
+ * 最多只能有一个作业处于FG状态。
  */
 
-/* Global variables */
-extern char **environ;      /* defined in libc */
-char prompt[] = "tsh> ";    /* command line prompt (DO NOT CHANGE) */
-int verbose = 0;            /* if true, print additional output */
-int nextjid = 1;            /* next job ID to allocate */
-char sbuf[MAXLINE];         /* for composing sprintf messages */
+/* 全局变量 */
+extern char **environ;      /* 在libc中定义 */
+char prompt[] = "tsh> ";    /* 命令行提示符（不要更改） */
+int verbose = 0;            /* 如果为真，打印额外的输出 */
+int nextjid = 1;            /* 下一个要分配的作业ID */
+char sbuf[MAXLINE];         /* 用于组合sprintf消息 */
 
-struct job_t {              /* The job struct */
-    pid_t pid;              /* job PID */
-    int jid;                /* job ID [1, 2, ...] */
-    int state;              /* UNDEF, BG, FG, or ST */
-    char cmdline[MAXLINE];  /* command line */
+struct job_t {              /* 作业结构 */
+    pid_t pid;              /* 作业PID */
+    int jid;                /* 作业ID [1, 2, ...] */
+    int state;              /* UNDEF, BG, FG, 或 ST */
+    char cmdline[MAXLINE];  /* 命令行 */
 };
-struct job_t jobs[MAXJOBS]; /* The job list */
-/* End global variables */
+struct job_t jobs[MAXJOBS]; /* 作业列表 */
+/* 结束全局变量 */
 
 
-/* Function prototypes */
+/* 函数原型 */
 
-/* Here are the functions that you will implement */
+/* 这里是你将要实现的函数 */
 void eval(char *cmdline);
 int builtin_cmd(char **argv);
 void do_bgfg(char **argv);
@@ -64,19 +64,19 @@ void sigchld_handler(int sig);
 void sigtstp_handler(int sig);
 void sigint_handler(int sig);
 
-/* Here are helper routines that we've provided for you */
-int parseline(const char *cmdline, char **argv); 
+/* 这里是我们为你提供的辅助程序 */
+int parseline(const char *cmdline, char **argv);
 void sigquit_handler(int sig);
 
 void clearjob(struct job_t *job);
 void initjobs(struct job_t *jobs);
-int maxjid(struct job_t *jobs); 
+int maxjid(struct job_t *jobs);
 int addjob(struct job_t *jobs, pid_t pid, int state, char *cmdline);
-int deletejob(struct job_t *jobs, pid_t pid); 
+int deletejob(struct job_t *jobs, pid_t pid);
 pid_t fgpid(struct job_t *jobs);
 struct job_t *getjobpid(struct job_t *jobs, pid_t pid);
-struct job_t *getjobjid(struct job_t *jobs, int jid); 
-int pid2jid(pid_t pid); 
+struct job_t *getjobjid(struct job_t *jobs, int jid);
+int pid2jid(pid_t pid);
 void listjobs(struct job_t *jobs);
 
 void usage(void);
@@ -86,164 +86,154 @@ typedef void handler_t(int);
 handler_t *Signal(int signum, handler_t *handler);
 
 /*
- * main - The shell's main routine 
+ * main - Shell的主程序
  */
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     char c;
     char cmdline[MAXLINE];
-    int emit_prompt = 1; /* emit prompt (default) */
+    int emit_prompt = 1; /* 发出提示（默认） */
 
-    /* Redirect stderr to stdout (so that driver will get all output
-     * on the pipe connected to stdout) */
+    /* 将stderr重定向到stdout（这样驱动程序将在连接到stdout的管道上获得所有输出） */
     dup2(1, 2);
 
-    /* Parse the command line */
+    /* 解析命令行 */
     while ((c = getopt(argc, argv, "hvp")) != EOF) {
         switch (c) {
-        case 'h':             /* print help message */
+        case 'h':             /* 打印帮助信息 */
             usage();
-	    break;
-        case 'v':             /* emit additional diagnostic info */
+        break;
+        case 'v':             /* 发出额外的诊断信息 */
             verbose = 1;
-	    break;
-        case 'p':             /* don't print a prompt */
-            emit_prompt = 0;  /* handy for automatic testing */
-	    break;
-	default:
+        break;
+        case 'p':             /* 不打印提示 */
+            emit_prompt = 0;  /* 对于自动测试很方便 */
+        break;
+    default:
             usage();
-	}
+    }
     }
 
-    /* Install the signal handlers */
+    /* 安装信号处理程序 */
 
-    /* These are the ones you will need to implement */
+    /* 这些是你将需要实现的 */
     Signal(SIGINT,  sigint_handler);   /* ctrl-c */
     Signal(SIGTSTP, sigtstp_handler);  /* ctrl-z */
-    Signal(SIGCHLD, sigchld_handler);  /* Terminated or stopped child */
+    Signal(SIGCHLD, sigchld_handler);  /* 子进程已终止或已停止 */
 
-    /* This one provides a clean way to kill the shell */
-    Signal(SIGQUIT, sigquit_handler); 
+    /* 这个提供了一种优雅的方式来杀死shell */
+    Signal(SIGQUIT, sigquit_handler);
 
-    /* Initialize the job list */
+    /* 初始化作业列表 */
     initjobs(jobs);
 
-    /* Execute the shell's read/eval loop */
+    /* 执行shell的读/评估循环 */
     while (1) {
 
-	/* Read command line */
-	if (emit_prompt) {
-	    printf("%s", prompt);
-	    fflush(stdout);
-	}
-	if ((fgets(cmdline, MAXLINE, stdin) == NULL) && ferror(stdin))
-	    app_error("fgets error");
-	if (feof(stdin)) { /* End of file (ctrl-d) */
-	    fflush(stdout);
-	    exit(0);
-	}
+    /* 读取命令行 */
+    if (emit_prompt) {
+        printf("%s", prompt);
+        fflush(stdout);
+    }
+    if ((fgets(cmdline, MAXLINE, stdin) == NULL) && ferror(stdin))
+        app_error("fgets错误");
+    if (feof(stdin)) { /* 文件结束（ctrl-d） */
+        fflush(stdout);
+        exit(0);
+    }
 
-	/* Evaluate the command line */
-	eval(cmdline);
-	fflush(stdout);
-	fflush(stdout);
-    } 
+    /* 评估命令行 */
+    eval(cmdline);
+    fflush(stdout);
+    fflush(stdout);
+    }
 
-    exit(0); /* control never reaches here */
+    exit(0); /* 控制永远不会到达这里 */
 }
   
-/* 
- * eval - Evaluate the command line that the user has just typed in
- * 
- * If the user has requested a built-in command (quit, jobs, bg or fg)
- * then execute it immediately. Otherwise, fork a child process and
- * run the job in the context of the child. If the job is running in
- * the foreground, wait for it to terminate and then return.  Note:
- * each child process must have a unique process group ID so that our
- * background children don't receive SIGINT (SIGTSTP) from the kernel
- * when we type ctrl-c (ctrl-z) at the keyboard.  
-*/
-void eval(char *cmdline) 
+/*
+ * eval - 评估用户刚刚输入的命令行
+ *
+ * 如果用户请求了一个内置命令（quit，jobs，bg或fg），则立即执行它。否则，fork一个子进程并在子进程的上下文中运行作业。如果作业在前台运行，等待它终止然后返回。注意：每个子进程必须有一个唯一的进程组ID，以便我们的后台子进程在我们在键盘上输入ctrl-c（ctrl-z）时不会从内核接收SIGINT（SIGTSTP）。
+ */
+void eval(char *cmdline)
 {
     return;
 }
 
-/* 
- * parseline - Parse the command line and build the argv array.
- * 
- * Characters enclosed in single quotes are treated as a single
- * argument.  Return true if the user has requested a BG job, false if
- * the user has requested a FG job.  
+/*
+ * parseline - 解析命令行并构建argv数组。
+ *
+ * 单引号内的角色被视为单个参数。如果用户请求了一个BG作业，则返回true，如果请求了一个FG作业，则返回false。
  */
-int parseline(const char *cmdline, char **argv) 
+int parseline(const char *cmdline, char **argv)
 {
-    static char array[MAXLINE]; /* holds local copy of command line */
-    char *buf = array;          /* ptr that traverses command line */
-    char *delim;                /* points to first space delimiter */
-    int argc;                   /* number of args */
-    int bg;                     /* background job? */
+    static char array[MAXLINE]; /* 保存命令行的本地副本 */
+    char *buf = array;          /* 遍历命令行的指针 */
+    char *delim;                /* 指向第一个空格分隔符 */
+    int argc;                   /* 参数数量 */
+    int bg;                     /* 后台作业？ */
 
     strcpy(buf, cmdline);
-    buf[strlen(buf)-1] = ' ';  /* replace trailing '\n' with space */
-    while (*buf && (*buf == ' ')) /* ignore leading spaces */
-	buf++;
+    buf[strlen(buf)-1] = ' ';  /* 将尾随的'\n'替换为空格 */
+    while (*buf && (*buf == ' ')) /* 忽略前导空格 */
+    buf++;
 
-    /* Build the argv list */
+    /* 构建argv列表 */
     argc = 0;
     if (*buf == '\'') {
-	buf++;
-	delim = strchr(buf, '\'');
+    buf++;
+    delim = strchr(buf, '\'');
     }
     else {
-	delim = strchr(buf, ' ');
+    delim = strchr(buf, ' ');
     }
 
     while (delim) {
-	argv[argc++] = buf;
-	*delim = '\0';
-	buf = delim + 1;
-	while (*buf && (*buf == ' ')) /* ignore spaces */
-	       buf++;
+    argv[argc++] = buf;
+    *delim = '\0';
+    buf = delim + 1;
+    while (*buf && (*buf == ' ')) /* 忽略空格 */
+           buf++;
 
-	if (*buf == '\'') {
-	    buf++;
-	    delim = strchr(buf, '\'');
-	}
-	else {
-	    delim = strchr(buf, ' ');
-	}
+    if (*buf == '\'') {
+        buf++;
+        delim = strchr(buf, '\'');
+    }
+    else {
+        delim = strchr(buf, ' ');
+    }
     }
     argv[argc] = NULL;
     
-    if (argc == 0)  /* ignore blank line */
-	return 1;
+    if (argc == 0)  /* 忽略空行 */
+    return 1;
 
-    /* should the job run in the background? */
+    /* 作业应该在后台运行吗？ */
     if ((bg = (*argv[argc-1] == '&')) != 0) {
-	argv[--argc] = NULL;
+    argv[--argc] = NULL;
     }
     return bg;
 }
 
-/* 
- * builtin_cmd - If the user has typed a built-in command then execute
- *    it immediately.  
+/*
+ * builtin_cmd - 如果用户输入了一个内置命令，则立即执行它。
  */
-int builtin_cmd(char **argv) 
+int builtin_cmd(char **argv)
 {
-    return 0;     /* not a builtin command */
+    return 0;     /* 不是一个内置命令 */
 }
 
-/* 
- * do_bgfg - Execute the builtin bg and fg commands
+/*
+ * do_bgfg - 执行内置的bg和fg命令
  */
-void do_bgfg(char **argv) 
+void do_bgfg(char **argv)
 {
     return;
 }
 
-/* 
- * waitfg - Block until process pid is no longer the foreground process
+/*
+ * waitfg - 阻塞直到进程pid不再是前台进程
  */
 void waitfg(pid_t pid)
 {
@@ -251,35 +241,30 @@ void waitfg(pid_t pid)
 }
 
 /*****************
- * Signal handlers
+ * 信号处理程序
  *****************/
 
-/* 
- * sigchld_handler - The kernel sends a SIGCHLD to the shell whenever
- *     a child job terminates (becomes a zombie), or stops because it
- *     received a SIGSTOP or SIGTSTP signal. The handler reaps all
- *     available zombie children, but doesn't wait for any other
- *     currently running children to terminate.  
+/*
+ * sigchld_handler - 每当一个子作业终止（变成僵尸），或者因为它接收到SIGSTOP或SIGTSTP信号而停止时，内核会向shell发送一个SIGCHLD。
+ * 处理程序收获所有可用的僵尸子作业，但不会等待任何其他当前运行的子作业终止。
  */
-void sigchld_handler(int sig) 
-{
-    return;
-}
-
-/* 
- * sigint_handler - The kernel sends a SIGINT to the shell whenver the
- *    user types ctrl-c at the keyboard.  Catch it and send it along
- *    to the foreground job.  
- */
-void sigint_handler(int sig) 
+void sigchld_handler(int sig)
 {
     return;
 }
 
 /*
- * sigtstp_handler - The kernel sends a SIGTSTP to the shell whenever
- *     the user types ctrl-z at the keyboard. Catch it and suspend the
- *     foreground job by sending it a SIGTSTP.  
+ * sigint_handler - 当用户在键盘上输入ctrl-c时，内核会向shell发送一个SIGINT。
+ * 捕获它并将SIGINT发送给前台作业。
+ */
+void sigint_handler(int sig)
+{
+    return;
+}
+
+/*
+ * sigtstp_handler - 当用户在键盘上输入ctrl-z时，内核会向shell发送一个SIGTSTP。
+ * 捕获它并通过发送SIGTSTP来暂停前台作业。
  */
 void sigtstp_handler(int sig) 
 {
@@ -480,26 +465,25 @@ void app_error(char *msg)
 }
 
 /*
- * Signal - wrapper for the sigaction function
+ * Signal - sigaction函数的包装器
  */
-handler_t *Signal(int signum, handler_t *handler) 
+handler_t *Signal(int signum, handler_t *handler)
 {
     struct sigaction action, old_action;
 
-    action.sa_handler = handler;  
-    sigemptyset(&action.sa_mask); /* block sigs of type being handled */
-    action.sa_flags = SA_RESTART; /* restart syscalls if possible */
+    action.sa_handler = handler;
+    sigemptyset(&action.sa_mask); /* 阻塞正在处理的信号类型 */
+    action.sa_flags = SA_RESTART; /* 如果可能，重启系统调用 */
 
     if (sigaction(signum, &action, &old_action) < 0)
-	unix_error("Signal error");
+        unix_error("信号错误");
     return (old_action.sa_handler);
 }
 
 /*
- * sigquit_handler - The driver program can gracefully terminate the
- *    child shell by sending it a SIGQUIT signal.
+ * sigquit_handler - 驱动程序可以通过发送SIGQUIT信号优雅地终止子shell。
  */
-void sigquit_handler(int sig) 
+void sigquit_handler(int sig)
 {
     printf("Terminating after receipt of SIGQUIT signal\n");
     exit(1);
